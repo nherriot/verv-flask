2da19de6de57982b5a7d0d0517cfb917809035c8
Nicholas Herriot <Nicholas.Herriot@gmail.com>
2019-08-10 16:22:21 +0100

    adding a simple hello world

---
+++ server/hello.py
@@ -0,0 +1,12 @@
+from flask import Flask, escape, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    name = request.args.get("name", "World")
+    return 'Hello, {}!'.format(name)
+
+
+app.run(host='0.0.0.0', port= 8000)
+
